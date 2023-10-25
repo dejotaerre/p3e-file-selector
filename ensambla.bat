99d490c0d3a326e098657641e19987fd24fd08e5
dejotaerre <dejotaerre@googlemail.com>
2023-10-24 21:50:06 -0300

    upd_wbat

--- ensambla.bat
+++ ensambla.bat
@@ -7,25 +7,37 @@ set fuse_exec=fusew\fuse
 
 .\bin\sjasmplus --lst --lstlab --raw=%nombre%.obj %nombre%.asm
 
-if errorlevel 1 (
-	echo.
-	echo "**************************"
-	echo "* Ensamblado incorrecto! *"
-	echo "**************************"
-	echo.
-	exit 1
-)
+if errorlevel 1 goto err_ensamblado
+goto next_test
 
+:err_ensamblado
+
+echo.
+echo "**************************"
+echo "* Ensamblado incorrecto! *"
+echo "**************************"
+echo.
+goto fin
+
+:next_test
 if exist %nombre%.bin del %nombre%.bin
 if exist %nombre%.dsk del %nombre%.dsk
+
+REM USO "specform" para crear una cabecera +3DOS a partir del archivo binario compilado por SJASMPLUS
+REM fue compilado para Windows 32 bits - buscar fuentes en https://www.seasip.info/ZX/unix.html del paquete
+REM TAPTOOLS, por si necesitas recompilarlo para usarlo en MSDOS
+REM (NOTA anecdótica: he comprobado que algunos AVs creen que SPECFORM es un virus, y nada mas lejos
+REM de la realidad..., es un falso positivo, ya que solo reescribe unos pocos bytes para crear la cabecera)
 
 .\bin\specform -a %org% %nombre%.obj
 ren %nombre%.obj.zxb %nombre%.bin
 
+echo.
+
 cd bin
 
 REM genero en un archivo los comandos para crear una imagen de disquete para testeos en el emulador junto
-REM al binario de esta utilidad con un cargador DISK para testeo, usando CPCXFS
+REM al binario de esta utilidad, con un cargador DISK para testeo, usando CPCXFS
 echo new -f PCW3 ..\selz80.dsk > makedsk
 echo open -f PCW3 ..\selz80.dsk >> makedsk
 echo put -f ..\disk DISK >> makedsk
@@ -46,13 +58,20 @@ echo put -f WECLEMAN.TAP >> makedsk
 rem echo put -f QUAZATRO.Z80" >> makedsk
 rem echo "put -f WHERETIM.TAP" >> makedsk
 
+REM USO CPCXFS para crear un DSK de testeo, está compilado para Windows 32bits, si vas
+REM a usar este BAT para testear en MSDOS vas a tener que descargar sus fuentes y 
+REM compilarlo, sin contar que tendrás que hacer mismo con SJASMPLUS y SPECFORM
+REM (no doy garantias que todo esto funcione en un ambiente MSDOS real, en el mejor de
+REM los casos solo podrás ensamblar las ROMs nada mas)
+
 cpcxfsw < makedsk
 
-del makedsk
+if exist makedsk del makedsk
+
 cd ..
 
-rem Hago una variable de entorno con los parámetros necesarios para ejecutar y testear
-rem esta utilidad
+REM Hago una variable de entorno con los parámetros necesarios para ejecutar y testear
+REM esta utilidad
 set fuse_opciones=--machine plus3
 set fuse_opciones=%fuse_opciones% --simpleide
 set fuse_opciones=%fuse_opciones% --multiface3
@@ -69,17 +88,20 @@ set fuse_opciones=%fuse_opciones% --drive-plus3a-type "Double-sided 80 track"
 set fuse_opciones=%fuse_opciones% --drive-plus3b-type "Double-sided 80 track"
 
 echo.
-echo.
 echo "***********************************************************************************************"
 echo EJECUTANDO:
 echo %fuse_exec% %fuse_opciones%
 echo "***********************************************************************************************"
 
-rem TODO pronto, ahora hay que probar con un emulador (FUSE en este caso)
+REM Está todo pronto, ahora hay que probar con un emulador (FUSE en este caso)
 %fuse_exec% %fuse_opciones%
 
-rem LIMPIEZA
+REM LIMPIEZA de archivos no necesarios posterior al testeo
 if exist %nombre%.bin del %nombre%.bin
 if exist %nombre%.dsk del %nombre%.dsk
 if exist %nombre%.lst del %nombre%.lst
 if exist %nombre%.obj del %nombre%.obj
+
+:fin
+echo.
+pause
